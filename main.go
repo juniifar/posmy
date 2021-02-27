cf9e574a861a2713a7c0c512e2d7704ecaaef355
Junifar Hidayat <junifar.hidayat@dhanapala.id>
2021-02-27 15:43:37 +0700

    Change random port

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 func main() {
@@ -11,9 +13,14 @@ func main() {
 		fmt.Fprintf(w, "Hello!")
 	})
 
+	port, err := strconv.Atoi(os.Getenv("PORT"))
+	if err == nil {
+		log.Fatal(err)
+	}
+
 	fmt.Printf("Starting server at port 80\n")
 
-	if err := http.ListenAndServe(":80", nil); err != nil {
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
 		log.Fatal(err)
 	}
 }
