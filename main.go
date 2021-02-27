bc5eec7219c96fd7fb24d66b85cfe1becfea1e22
Junifar Hidayat <junifar.hidayat@dhanapala.id>
2021-02-27 16:08:17 +0700

    test lagi

--- main.go
+++ main.go
@@ -12,13 +12,6 @@ func main() {
 		fmt.Fprintf(w, "Hello!")
 	})
 
-	// fmt.Println("Getting PORT First")
-	// port, err := strconv.Atoi(os.Getenv("PORT"))
-	// if err == nil {
-	// 	fmt.Println("Error Getting PORT ??")
-	// 	log.Fatal(err)
-	// }
-
 	fmt.Printf("Starting server at port %s\n", os.Getenv("PORT"))
 
 	if err := http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), nil); err != nil {
